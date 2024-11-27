b53bfbf29dbb521fe531da14d9541d4b4190e0ed
Juan <juanwcaraujo28@gmail.com>
2024-11-27 16:16:24 -0300

    Ajustes

--- src/test/java/br/com/aurora/drograria/dao/CidadeDAOTest.java
+++ src/test/java/br/com/aurora/drograria/dao/CidadeDAOTest.java
@@ -17,44 +17,57 @@ public class CidadeDAOTest {
 		EstadoDao estadoDao = new EstadoDao(null);
 		Estado estado = estadoDao.buscar(2L);
 		Cidade cidade = new Cidade();
-		cidade.setNome("Salvador");
+		cidade.setNome("");
 		cidade.setEstado(estado);
 		CidadeDao cidadeDao = new CidadeDao(null);
 		cidadeDao.salvar(cidade);
 	}
+
 	@Test
 	public void listar() {
 		CidadeDao cidadeDao = new CidadeDao(null);
 		List<Cidade> lista = cidadeDao.listar();
 		for (Cidade cidade : lista) {
-			System.out.println(cidade.getNome()+"-"+cidade.getEstado().getNome());
+			System.out.println("Codigo da cidade: " + cidade.getCodigo());
+			System.out.println(cidade.getNome() + "-" + cidade.getEstado().getNome());
+			System.out.println("");
 		}
-		}
+	}
+
 	@Test
 	@Ignore
 	public void excluir() {
 		long codigo = 11L;
 		CidadeDao cidadeDao = new CidadeDao(null);
 		Cidade cidade = cidadeDao.buscar(codigo);
-		if(cidade == null) {
+		if (cidade == null) {
 			System.out.println("A cidade não foi encontrada");
-		}else{
+		} else {
+			System.out.println("Antes da remoção:");
+			listar();
 			cidadeDao.excluir(cidade);
 			System.out.println("A cidade foi excluida");
+			System.out.println("Apos remoção:");
 			listar();
 		}
-		}
+	}
+
 	@Test
 	@Ignore
 	public void editar() {
 		long codigo = 8L;
 		CidadeDao cidadeDao = new CidadeDao(null);
 		Cidade cidade = cidadeDao.buscar(codigo);
-		if(cidade == null) {
+		if (cidade == null) {
 			System.out.println("A cidade não foi encontrada");
-		}else {
+		} else {
+			System.out.println("Antes da remoção:");
+			listar();
 			cidade.setEstado(null);
-			cidade.setNome(null);	
+			cidade.setNome(null);
+			System.out.println("Apos a remoção: ");
+			listar();
+			 
 		}
 	}
 }
